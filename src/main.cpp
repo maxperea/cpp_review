024ab21d4b490ca16e8d7e4aa995f7577ced323a
Max Perea Düring <maxpereaduring@gmail.com>
2023-05-26 17:38:53 +0200

    print vec

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
-#include "lib.hpp"
 #include <iostream>
+#include <vector>
+
+
+template <typename T>
+void print_vec(std::vector<T> &vect) {
+  for (auto element : vect) {
+    std::cout << element << " ";
+  }
+  std::cout << "\n";
+}
+
+template <typename T>
+void quick_sort(std::vector<T> &vector) {
+  return;
+}
 
 int main(int argc, char **argv) {
-  std::cout << "Hello World!\n";
+
+  std::vector<int> vect{ 30, 10, 20 };
+
+  print_vec(vect);
+  quick_sort(vect);
+  print_vec(vect);
+
   return 0;
 }
