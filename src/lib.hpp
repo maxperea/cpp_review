3b85082559e5124ac4d3c0642289ec9fb92a6219
Max Perea Düring <maxpereaduring@gmail.com>
2023-05-29 21:16:39 +0200

    Checkpoint

--- src/lib.hpp
+++ src/lib.hpp
@@ -1,1 +1,16 @@
 // Empty lib
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+template <typename Iterator>
+void quicksort_aux(Iterator begin, Iterator end) {
+  std::cout << *begin;
+  if (*begin == *end) {
+   return;
+  }
+  auto pivot = [&end] (const auto& a) { return a < *end;};
+  auto new_end = std::partition(begin, end, pivot);
+  quicksort_aux(begin, new_end);
+  quicksort_aux(new_end, end);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,129 @@
 #include <iostream>
 #include <vector>
 
+struct AbstractPrintableT {
+  virtual ~AbstractPrintableT() = default;
+  virtual void print_static() = 0;
+};
 
 template <typename T>
-void print_vec(std::vector<T> &vect) {
-  for (auto element : vect) {
-    std::cout << element << " ";
+concept Printable = requires(T t) {
+  t.print_static();
+};
+
+template <typename T>
+struct PrintableT : AbstractPrintableT {
+  void print_static() override {
+    static_cast<T*>(this)->print_implementation();
   }
-  std::cout << "\n";
+};
+
+struct PrintableD {
+  virtual ~PrintableD() = default;
+  virtual void print_dynamic() = 0;
+};
+
+
+struct Cat : PrintableT<Cat> {
+  void print_implementation() {
+    std::cout << "Static hello from Cat\n";
+  }
+};
+
+struct Dog : PrintableT<Dog> , PrintableD {
+  void print_implementation() {
+    std::cout << "Static hello from Dog\n";
+  }
+
+  void print_dynamic() override {
+    std::cout << "Dynamic hello from Dog\n";
+  }
+};
+
+struct SmallDog {
+  void print_static() {
+    std::cout << "WOOF!\n";
+  }
+};
+
+struct SmallCat {
+  void print_static() {
+    std::cout << "MIAO";
+  }
+};
+
+int add (int a, int b) {
+  return a + b;
 }
 
+int say_hello(int i) {
+  return i + i;
+}
+
+// template<typename T, typename Unused = typename std::enable_if<std::is_base_of<PrintableD, T>::value>::type>
+// void printSomething(T* c) {
+//   c->print_dynamic();
+// }
+
+template<Printable T>
+void printSomething(T& c) {
+  c.print_static();
+};
+
 template <typename T>
-void quick_sort(std::vector<T> &vector) {
-  return;
+void print_vector(std::vector<T> vec) {
+  for (auto& elem : vec) {
+    std::cout << elem << '\n';
+  }
+}
+
+
+#include <algorithm>
+
+template <typename Iterator>
+void quicksort_aux(Iterator begin, Iterator end) {
+  if (begin == end) {
+   return;
+  }
+  auto lower = [&end] (const auto& a) { return a < *end;};
+  auto higher = [&end] (const auto& a) { return a > *end;};
+
+  auto new_end = std::partition(begin, end, lower);
+  auto new_begin = std::partition(new_end, end, higher);
+
+  quicksort_aux(begin, new_end);
+  quicksort_aux(new_begin, end);
 }
 
 int main(int argc, char **argv) {
 
-  std::vector<int> vect{ 30, 10, 20 };
+  SmallDog first_small_dog;
+  printSomething(first_small_dog);
 
-  print_vec(vect);
-  quick_sort(vect);
-  print_vec(vect);
+  SmallCat first_small_cat;
+  printSomething(first_small_cat);
 
-  return 0;
+  auto first_cat = std::make_unique<Cat>();
+  auto first_dog = std::make_unique<Dog>();
+
+  std::vector<std::unique_ptr<AbstractPrintableT>> stat_arr = {};
+  stat_arr.push_back(std::move(first_dog));
+  stat_arr.push_back(std::move(first_cat));
+
+  for (auto &elem : stat_arr) {
+    elem->print_static();
+  }
+
+  auto second_dog = std::make_unique<Dog>();
+  std::vector<std::unique_ptr<PrintableD>> dyn_arr = {};
+
+  dyn_arr.push_back(std::move(second_dog));
+
+  for (auto &elem : dyn_arr) {
+    elem->print_dynamic();
+  }
+
+  std::vector<int> test_array = {1,5,2,4,5,3};
+  quicksort_aux(test_array.begin(), test_array.end());
+  print_vector(test_array);
 }
