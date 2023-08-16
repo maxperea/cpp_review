deffb406686b8d8237efa3b2d8d49848fbafa8d8
Max Perea Düring <maxpereaduring@gmail.com>
2023-08-16 14:29:41 +0200

    Something

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,7 @@ struct AbstractPrintableT {
 };
 
 template <typename T>
-concept Printable = requires(T t) {
-  t.print_static();
-};
+concept Printable = requires(T t) { t.print_static(); };
 
 template <typename T>
 struct PrintableT : AbstractPrintableT {
@@ -24,44 +22,29 @@ struct PrintableD {
   virtual void print_dynamic() = 0;
 };
 
-
 struct Cat : PrintableT<Cat> {
-  void print_implementation() {
-    std::cout << "Static hello from Cat\n";
-  }
+  void print_implementation() { std::cout << "Static hello from Cat\n"; }
 };
 
-struct Dog : PrintableT<Dog> , PrintableD {
-  void print_implementation() {
-    std::cout << "Static hello from Dog\n";
-  }
+struct Dog : PrintableT<Dog>, PrintableD {
+  void print_implementation() { std::cout << "Static hello from Dog\n"; }
 
-  void print_dynamic() override {
-    std::cout << "Dynamic hello from Dog\n";
-  }
+  void print_dynamic() override { std::cout << "Dynamic hello from Dog\n"; }
 };
 
 struct SmallDog {
-  void print_static() {
-    std::cout << "WOOF!\n";
-  }
+  void print_static() { std::cout << "WOOF!\n"; }
 };
 
 struct SmallCat {
-  void print_static() {
-    std::cout << "MIAO";
-  }
+  void print_static() { std::cout << "MIAO\n"; }
 };
 
-int add (int a, int b) {
-  return a + b;
-}
+int add(int a, int b) { return a + b; }
 
-int say_hello(int i) {
-  return i + i;
-}
+int say_hello(int i) { return i + i; }
 
-template<Printable T>
+template <Printable T>
 void printSomething(T& c) {
   c.print_static();
 };
@@ -78,17 +61,41 @@ void qsort(Iter begin, Iter end) {
   if (end - begin <= 1) return;
 
   auto pivot = end - 1;
-  auto compare = [pivot](const auto& a) { return a < *pivot;};
-  auto mid = std::partition(begin, end, compare);
+  auto compare = [pivot](const auto& a) { return a < *pivot; };
+  auto mid = my_partition(begin, end, compare);
   std::swap(*mid, *pivot);
 
   qsort(begin, mid);
   qsort(mid + 1, end);
 }
 
-int main(int argc, char **argv) {
-  std::vector<int> test_array = {1,5,2,4,5,3};
-  qsort(test_array.begin(), test_array.end());
-  print_vector(test_array);
-  std::sort(test_array.begin(), test_array.end());
+template <typename Iter, typename Predicate>
+Iter my_partition(Iter start, Iter end, Predicate pred) {
+  while (start < end) {
+    if (pred(*start)) {
+      start++;
+    } else {
+      std::swap(*start, *(end - 1));
+      end--;
+    }
+  }
+  return end;
+}
+
+int test_fun() {
+  int a = 5;
+  return ++a;
+}
+
+int main(int argc, char** argv) {
+  // std::vector<int> test_array = {10, 5, 20, 201, 12, 7, 9, 9};
+  // qsort(test_array.begin(), test_array.end());
+  // print_vector(test_array);
+  // std::sort(test_array.begin(), test_array.end());
+  std::string test = argv[1];
+  auto test_string = std::string(test);
+  std::cout << test << "\n";
+  std::cout << test_string << "\n";
+
+  std::cout << test_fun() << "\n";
 }
